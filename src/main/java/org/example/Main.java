3861da0f17768d305eedac2deba1a92847d7bfcf
1 <vadimvetrov2015@mail.ru>
2023-04-24 15:53:43 +1000

    First part

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/new_schema", "root", "root")) {
+            Statement stmt = connection.createStatement();
+//            String sqlCommand = "CREATE TABLE animal" +
+//                    "(id INTEGER not NULL, " +
+//                    " name VARCHAR(255), " +
+//                    " kind VARCHAR(255), " +
+//                    " age INTEGER, " +
+//                    " PRIMARY KEY ( id ))";
+//            stmt.executeUpdate(sqlCommand);
+//            String insertCommand = "INSERT INTO animal(id, name, kind, age) VALUES(7, 'Fish', 'sturgeon',5);";
+//            int i = stmt.executeUpdate(insertCommand);
+//            System.out.println("Успешно применена " + i + " строка");
+              String selectCommand = "SELECT * FROM animal ;";
+              ResultSet resultSet = stmt.executeQuery(selectCommand);
+//            System.out.println(resultSet.toString());
+
+
+            while (resultSet.next()){
+                System.out.print(resultSet.getString(1) + " ");
+                System.out.print(resultSet.getString(2) + " ");
+                System.out.print(resultSet.getString(3) + " ");
+                System.out.print(resultSet.getString(4) + " ");
+                System.out.println();
+            }
+//            String delCommand = "Delete from animal where id = 7;";
+//            stmt.execute(delCommand);
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
+
